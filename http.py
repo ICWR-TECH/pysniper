a18cb62e80c29aeda532ef34b4753a8254737f39
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-05-06 23:48:15 +0700

    Add files via upload

---
+++ http.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Coded By Afrizal F.A - ICWR-TECH
+import sys, datetime
+from scapy.all import *
+from scapy.error import Scapy_Exception
+m_iface=sys.argv[1]
+filter_message="http"
+count=0
+def pktTCP(pkt):
+    global count
+    if pkt.haslayer(TCP) and pkt.getlayer(TCP).dport == 80 and pkt.haslayer(Raw):
+            count=count+1
+	    print "[+] Recive Packet\n"
+            paket = pkt.getlayer(Raw).load
+            print paket
+	    f = open("sniff-" + datetime.today().strftime('%Y-%m-%d') + ".txt", "a")
+	    f.write(paket)
+	    print "[+] End Of Packet\n"
+sniff(iface=m_iface,prn=pktTCP)

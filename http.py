2cdc4e53bff6a2d2abc5f4ca02f3f75582001016
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-10-08 00:11:08 +0700

    Update http.py

--- http.py
+++ http.py
@@ -6,13 +6,13 @@ from scapy.error import Scapy_Exception
 m_iface=sys.argv[1]
 count=0
 def pktTCP(pkt):
-    global count
-    if pkt.haslayer(TCP) and pkt.getlayer(TCP).dport == 80 and pkt.haslayer(Raw):
-            count=count+1
-	    print("[+] Recive Packet\n")
-            paket = pkt.getlayer(Raw).load
-            print paket
-	    f = open("sniff-" + datetime.today().strftime('%Y-%m-%d') + ".txt", "a")
-	    f.write(paket)
-	    print("[+] End Of Packet\n")
+	global count
+	if pkt.haslayer(TCP) and pkt.getlayer(TCP).dport == 80 and pkt.haslayer(Raw):
+		count=count+1
+		print("[+] Recive Packet\n")
+		paket = pkt.getlayer(Raw).load
+		print(paket)
+		f = open("sniff-" + datetime.today().strftime('%Y-%m-%d') + ".txt", "a")
+		f.write(paket)
+		print("[+] End Of Packet\n")
 sniff(iface=m_iface,prn=pktTCP)

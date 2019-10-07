044c86e97c7a3edd5cae681e149b72a25c3a3c7b
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-10-08 00:08:19 +0700

    Update http.py

--- http.py
+++ http.py
@@ -9,10 +9,10 @@ def pktTCP(pkt):
     global count
     if pkt.haslayer(TCP) and pkt.getlayer(TCP).dport == 80 and pkt.haslayer(Raw):
             count=count+1
-	    print "[+] Recive Packet\n"
+	    print("[+] Recive Packet\n")
             paket = pkt.getlayer(Raw).load
             print paket
 	    f = open("sniff-" + datetime.today().strftime('%Y-%m-%d') + ".txt", "a")
 	    f.write(paket)
-	    print "[+] End Of Packet\n"
+	    print("[+] End Of Packet\n")
 sniff(iface=m_iface,prn=pktTCP)

351afddbf81385b326ea833ab802061da53a326e
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-08-27 14:33:44 +0700

    Update http.py

--- http.py
+++ http.py
@@ -1,20 +1,38 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Coded By Afrizal F.A - ICWR-TECH
-import sys, datetime
+
+import os, datetime
 from scapy.all import *
 from scapy.error import Scapy_Exception
-m_iface=sys.argv[1]
-count=0
-def pktTCP(pkt):
-	global count
-	if pkt.haslayer(TCP) and pkt.getlayer(TCP).dport == 80 and pkt.haslayer(Raw):
- 		count=count+1
-		try:
-			print("[+] Recive Packet -> " + pkt.getlayer(IP).src + " [+]\n")
-			paket = pkt.getlayer(Raw).load
-			print(paket.decode('utf-8') + "\n")
-			f = open("sniff-" + datetime.today().strftime('%Y-%m-%d') + ".txt", "a")
-			f.write(paket.decode('utf-8'))
-		except:
-			pass
-sniff(iface=m_iface,prn=pktTCP)
+from argparse import ArgumentParser
+
+class sniffer:
+
+    def pktTCP(self, pkt):
+
+        if pkt.haslayer(TCP) and pkt.getlayer(TCP).dport == 80 and pkt.haslayer(Raw):
+
+            try:
+
+                print("[+] [Recive Packet] [{}]\n".format(pkt.getlayer(IP).src))
+                paket = pkt.getlayer(Raw).load
+                print(paket.decode('utf-8') + "\n")
+                f = open("result-sniffing/sniff-{}.txt".format(datetime.today().strftime('%Y-%m-%d')), "a")
+                f.write(paket.decode('utf-8'))
+
+            except:
+
+                pass
+
+    def __init__(self):
+
+        if not os.path.isdir("result-sniffing"):
+
+            os.mkdir("result-sniffing")
+
+        parser = ArgumentParser()
+        parser.add_argument("-i", "--interface", required=True)
+        args = parser.parse_args()
+        sniff(iface=args.interface, prn=self.pktTCP)
+
+sniffer()
